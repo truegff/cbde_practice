d4c093904f898b77d87b442a780f388703f85f7b
Alexander Dolidze <alexander.dolidze@gmail.com>
2022-12-01 04:37:55 +0400

    fixed supply chain test

--- 02_supply_chain_app/test/supply_chain.js
+++ 02_supply_chain_app/test/supply_chain.js
@@ -12,27 +12,33 @@ contract("SupplyChain", function (/* accounts */) {
   });
 });
 
+async function invoke(contract, methodName, ...parameters) {
+    let method = contract[methodName];
+    let result = await method.call.apply(this, parameters);
+    await method.apply(this, parameters);
+    return result;
+}
 
 contract("SupplyChain", async accounts => {
   it('should create a Participant', async () => {
     let instance;
-    let participantId;
+    let participantIdBN;
     let participant;
 
     instance = await SupplyChain.deployed();
 
-    participantId = await instance.addParticipant("A", "passA", "0xccc00f129AD3316defdE4E1bb4c33B41aEC9be30", "Manufacturer");
-    participant = await instance.participants(participantId);
+    participantIdBN = await invoke(instance, 'addParticipant', "A", "passA", "0xccc00f129AD3316defdE4E1bb4c33B41aEC9be30", "Manufacturer");
+    participant = await instance.participants(participantIdBN.toNumber());
     assert.equal(participant[0], "A");
     assert.equal(participant[2], "Manufacturer");
 
-    participantId = await instance.addParticipant("B", "passB", "0x1f0fa5e75e69653c39d5c60bdA713BFF3F3bA5b1", "Supplier");
-    participant = await instance.participants(participantId);
+    participantIdBN = await invoke(instance, 'addParticipant', "B", "passB", "0x1f0fa5e75e69653c39d5c60bdA713BFF3F3bA5b1", "Supplier");
+    participant = await instance.participants(participantIdBN.toNumber());
     assert.equal(participant[0], "B");
     assert.equal(participant[2], "Supplier");
 
-    participantId = await instance.addParticipant("C", "passC", "0x0925D81C9d639b8994ea74330AC24ca72d6135d6", "Consumer");
-    participant = await instance.participants(participantId);
+    participantIdBN = await invoke(instance, 'addParticipant', "C", "passC", "0x0925D81C9d639b8994ea74330AC24ca72d6135d6", "Consumer");
+    participant = await instance.participants(participantIdBN.toNumber());
     assert.equal(participant[0], "C");
     assert.equal(participant[2], "Consumer");
   });
